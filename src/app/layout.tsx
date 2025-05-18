5a922aa2948274a9a324eac629da795f65aa4bdd
USUARIO <glogic9605@gmail.com>
2025-05-18 11:19:16 -0500

    Cambiamos el h-full del body por h-screen para ver si esto corrige el escalado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
        {/* <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head> */}
-      <body className="h-full bg-[#0C1A1A] text-[#6ACFC7] overflow-hidden">
+      <body className="h-screen bg-[#0C1A1A] text-[#6ACFC7] overflow-hidden">
        
           {children}
        
