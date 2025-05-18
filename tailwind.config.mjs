f7ff8c44201bd7d07dd3cf7fb791c48b85738441
USUARIO <glogic9605@gmail.com>
2025-05-18 11:59:39 -0500

    Aun persiste el error de escalado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
+//src/tailwind.config.msj
 /** @type {import('tailwindcss').Config} */
 const config = {
+    // mode:"jit",
     content: [
       "./app/**/*.{js,ts,jsx,tsx}",
       "./src/**/*.{js,ts,jsx,tsx}",
+      // "./components/**/*.{js,ts,jsx,tsx}",
       "./public/**/*.html"
     ],
     theme: {
